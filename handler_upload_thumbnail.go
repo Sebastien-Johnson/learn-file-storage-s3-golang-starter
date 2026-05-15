ac7c86d581bc4968ce4ec4d0b2ca9f641ca79d17
Sebastien-Johnson <sebastien.z.johnson@gmail.com>
2026-05-15 01:24:02 -0400

    Partway through 5.5

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"time"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
+	const maxMemory = 10 << 20
+	err := r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse multiple data points", err)
+		return
+	}
+
 	videoIDString := r.PathValue("videoID")
 	videoID, err := uuid.Parse(videoIDString)
 	if err != nil {
@@ -28,10 +38,53 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	fileData, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't get thumbnail", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	mediaType := fileHeader.Header.Get("Content-Type")
+
+	imgData, err := io.ReadAll(fileData)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't get thumbnail", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't get video", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't get video", err)
+		return
+	}
+
+	newThumbnail := thumbnail{
+		data:      imgData,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoID] = newThumbnail
+
+	err = cfg.db.UpdateVideo(database.Video{
+		ID:           videoID,
+		CreatedAt:    video.CreatedAt,
+		UpdatedAt:    time.Now(),
+		ThumbnailURL: fmt.Sprintf("http://localhost:%d/api/thumbnails/%s", cfg.port, videoID),
+		VideoURL:     video.VideoURL,
+	})
+
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
